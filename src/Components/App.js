b9634e00f072c33199bb0009e39155c1ed764bda
jules <judigonnet@gmail.com>
2024-03-07 20:00:51 +0100

    Gagnez en temps et en efficacité grâce aux listes et aux conditions

---
+++ src/Components/App.js
@@ -0,0 +1,9 @@
+import Banner from './Banner'
+import Card from './Card'
+import ShoppingList from "./ShoppingList";
+function App() {
+  return <div><Banner/><Card/><ShoppingList/></div>
+
+}
+
+export default App
