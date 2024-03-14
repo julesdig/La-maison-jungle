b63c7969412174ba533f2a630eab64ca00d51094
jules <judigonnet@gmail.com>
2024-03-14 16:09:35 +0100

    Partagez votre state entre différents composants

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import Banner from './Banner'
 import logo from '../assets/logo.png'
 import Cart from './Cart'
@@ -6,19 +7,20 @@ import ShoppingList from './ShoppingList'
 import '../styles/Layout.css'
 
 function App() {
-  return (
-      <div>
-        <Banner>
-          <img src={logo} alt='La maison jungle' className='lmj-logo' />
-          <h1 className='lmj-title'>La maison jungle</h1>
-        </Banner>
-        <div className='lmj-layout-inner'>
-          <Cart />
-          <ShoppingList />
+    const [cart, updateCart] = useState([])
+    return (
+        <div>
+            <Banner>
+                <img src={logo} alt='La maison jungle' className='lmj-logo' />
+                <h1 className='lmj-title'>La maison jungle</h1>
+            </Banner>
+            <div className='lmj-layout-inner'>
+                <Cart cart={cart} updateCart={updateCart} />
+                <ShoppingList cart={cart} updateCart={updateCart} />
+            </div>
+            <Footer />
         </div>
-        <Footer />
-      </div>
-  )
+    )
 }
 
 export default App
